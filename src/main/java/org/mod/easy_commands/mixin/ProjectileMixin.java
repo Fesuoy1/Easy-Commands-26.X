22985890530da5874a914ce5426e1415ee189297
Fesuoy1 <Fesuoy1>
2026-06-27 01:07:26 +0300

    fix: explosive projectiles now properly show particles (and disabled for performance reasons) release: bump version to 2.1.0

--- src/main/java/org/mod/easy_commands/mixin/ProjectileMixin.java
+++ src/main/java/org/mod/easy_commands/mixin/ProjectileMixin.java
@@ -1,6 +1,8 @@
 package org.mod.easy_commands.mixin;
 
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
@@ -8,6 +10,7 @@ import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.mod.easy_commands.ModGameRules;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -33,12 +36,23 @@ public class ProjectileMixin {
         }
     }
 
+    @Unique
+    private static final boolean particlesEnabled = false;
+
     @Inject(at = @At("HEAD"), method = "tick")
     public void onTick(CallbackInfo ci) {
-        Level level = ((Projectile) (Object) this).level();
-        if (ModGameRules.isExplosiveProjectilesEnabled(level)) {
-            Vec3 pos = ((Projectile) (Object) this).position();
-            level.addParticle(ParticleTypes.SMOKE, pos.x(), pos.y() + 0.1, pos.z(), 0.0, 0.0, 0.0);
+        // For now, disable particles since they lag the server if there are too many
+        if (!particlesEnabled) return;
+
+        Projectile projectile = (Projectile) (Object) this;
+        Level level = projectile.level();
+        if (ModGameRules.isExplosiveProjectilesEnabled(level)) { // already checks if it's a server, etc.
+            Vec3 pos = projectile.position();
+            boolean isOwnerPlayer = projectile.getOwner() instanceof ServerPlayer;
+            if (isOwnerPlayer)
+                ((ServerLevel)level).sendParticles((ServerPlayer)projectile.getOwner(), ParticleTypes.SMOKE, false, false, pos.x(), pos.y() + 0.1, pos.z(), 1, 0.0, 0.0, 0.0, 0.0);
+            else
+                ((ServerLevel)level).sendParticles(ParticleTypes.SMOKE, pos.x(), pos.y() + 0.1, pos.z(), 1, 0.0, 0.0, 0.0, 0.0);
         }
     }
 }
